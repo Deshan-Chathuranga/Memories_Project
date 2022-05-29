de141db6cb77858adb6736aec9bc7bddf4f7027d
Deshan Chathuranga <deshanc321@gmail.com>
2022-05-29 23:03:26 +0530

    Modified DB url

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 
 //Import all routes
@@ -19,11 +20,13 @@ app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use(cors());
 
-const CONNECTION_URL='mongodb+srv://deshbro1998:deshbro1998@cluster0.f4c3i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+dotenv.config();
+
+const CONNECTION_URL=process.env.DB_URL;
 
 const PORT=process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:true})
    .then(()=>app.listen(PORT,()=>console.log(`Server is running on port: ${PORT}`)))
    .catch((error)=>console.log(error.message));
 
